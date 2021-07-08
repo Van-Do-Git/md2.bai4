cf7ee499a33041b2687080d748496dd1ff6ebc92
unknown <mittervan@gmail.com>
2021-07-08 13:42:50 +0700

    fix agument the excercise StopWatch

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,35 +1,55 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class StopWatch {
     public static void main(String[] args) {
         StopWatch example = new StopWatch();
-        System.out.println("Start coutn TimeMillis in:\t" + example.startTime);
-        Scanner input = new Scanner(System.in);
-        int confirm;
-        do {
-            System.out.println("Do you want to stop coutn TimeMillis? input 1 to stop; input 2 to resset");
-            confirm = input.nextInt();
-            if(confirm==2){
-                example.start();
-                System.out.println("\"Reset start coutn TimeMillis in:\t"+example.startTime);
+        int[] arr = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            arr[i]=(int) Math.round(Math.random()*99+1);
+        }
+        int temp = 0;
+        System.out.println("Start agument selection sort in:\t" +example.startTime);
+        for (int i = 0; i < 100000; i++) {
+            for (int j = i; j <100000 ; j++) {
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
             }
-        } while (confirm != 1);
+        }
         example.stop();
-        System.out.println("Total ElapsedTime is:\t" + example.getElapsedTime());
+        System.out.println("Total times enforcement the agument sort is:\t"+example.getElapsedTime());
+//        StopWatch example = new StopWatch();
+//        System.out.println("Start coutn TimeMillis in:\t" + example.startTime);
+//        Scanner input = new Scanner(System.in);
+//        int confirm;
+//        do {
+//            System.out.println("Do you want to stop coutn TimeMillis? input 1 to stop; input 2 to resset");
+//            confirm = input.nextInt();
+//            if(confirm==2){
+//                example.start();
+//                System.out.println("\"Reset start coutn TimeMillis in:\t"+example.startTime);
+//            }
+//        } while (confirm != 1);
+//        example.stop();
+//        System.out.println("Total ElapsedTime is:\t" + example.getElapsedTime());
     }
 
     private long startTime, endTime;
 
     public StopWatch() {
-        startTime = System.currentTimeMillis();
+        startTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
     }
 
     public void start() {
-        startTime = System.currentTimeMillis();
+        startTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
     }
 
     public void stop() {
-        endTime = System.currentTimeMillis();
+        endTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
     }
 
     public long getElapsedTime() {
